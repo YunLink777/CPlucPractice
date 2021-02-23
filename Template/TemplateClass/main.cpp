8b39d125a4b70b97240675fd263b5ba3b6ca30c8
YunLink <daxianlcl@foxmail.com>
2021-02-23 19:54:57 +0800

    增加模板类demo内容简要

--- Template/TemplateClass/main.cpp
+++ Template/TemplateClass/main.cpp
@@ -5,6 +5,12 @@
  * \date 2021/2/23
  *
  * \brief 模板类相关demo
+ *  \content:
+ * 1、模板类基础；2、成员函数实例化时机；3、concept和static_assert
+ * 4、模板类的友元函数和友元类（两种声明方式区别）
+ * 5、模板类的全特化、半特化、半特化为另一个模板类
+ * 6、默认模板参数；7、using、typedef、typename别名
+ * 7、deduced guides 推断指导规则
  */
 
 #include <iostream>
